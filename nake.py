18e6eb52821cb02ee573fb3f2c723b341a32dd41
BOSOEK <thydcj@naver.com>
2020-10-27 21:20:17 +0900

    주식 게임만들기 2틀째 : 주식 증감 출력 조정 및 판매 기능만 완성하면 됨

--- nake.py
+++ nake.py
@@ -6,25 +6,24 @@
 from random import *
 
 class Company :
+    name = ""
     def __init__(self, name) :
         self.name = name
-    money = 9999
-    upDown = 0.0
-    mose = ""
+    money = 1000
+    upDown = 0
+    upDownMoney = 0
+    mose = 0
     def change(self) :
-        upDown = randrange(0, 201)
-        mosechoice = randint(0, 2)
-        if mose == 0 : 
-            self.money = self.money - upDown
-            mose = "-" + upDown
-        else :
-            self.money = self.money + upDown
-            mose = "+" + upDown
+        self.upDown = randrange(-500, 501)
+        self.money = self.money + self.upDown
+        self.upDownMoney = 1000 - self.money
+        self.upDownMoney = self.upDownMoney * (-1)
     buyCheck = 0
     buyCompany = 0
     def Buy(self, buyCompany) :
         self.buyCheck = self.upDown
-        self.buyCompany = buyCompany
+        self.buyCompany = self.buyCompany + buyCompany
+
     
 
 companys = []
@@ -34,7 +33,7 @@ companys.append(Company("하이마트"))
 companys.append(Company("이마트"))
 companys.append(Company("구글"))
 
-mymoney = 100000
+mymoney = 2500
 
 # 튜토리얼 출력
 def tutorial():
@@ -45,33 +44,29 @@ def tutorial():
     print("< buy 명령을 입력하시면 회사 선택창이 나오고 선택하시면 회사의 주식을 사실 수 있습니다 >")
     print("< sell 명령을 입력하시면 회사 선택창이 나오고 선택하시면 회사의 주식을 판매 하실 수 있습니다 >")
     answer = input("* 게임을 시작하시겠습니까?(y/n) >> ")
-    try :
-        if answer == 'n' :
-            raise ValueError
-        elif answer != 'y' :
-            raise ValueError
-        
-    except ValueError :
-        print("< 이용해 주셔서 감사합니다 >")
+      
+    if answer != 'y' :
+        return "err"
 
 def Companys() :
     print()
     print("< 현재 회사 정보 >")
     for i in range(5) :
         companys[i].change()
-        print(companys[i].name + " 현재 주식 : " + companys[i].money + " 증감률 : " + companys[i].mose)
+        print(companys[i].name + " 현재 주식 : " + str(companys[i].money) + " 증감률 : {0:+}".format(companys[i].upDownMoney))
 
 def Buy() :
     print()
     global mymoney
     print("< 살 회사를 선택하여 주세요 >")
     for i in range(5) :
-        print(i+1 + " : " + companys[i].name)
-    choose = input(">>")
+        print(str(i+1) + " : " + companys[i].name)
+    choose = int(input(">>"))
     if choose <=0 and choose >5 :
         print("< 지정되지 않은 회사입니다 >")
     else :
-        if choose == '1' :
+        global mymoney
+        if choose == 1 :
             buyMoney = int(input("사실 금액을 적어주세요 >> "))
             if mymoney < buyMoney :
                 print("< 보유 금액이 부족합니다 >")
@@ -79,139 +74,67 @@ def Buy() :
                 mymoney = mymoney - buyMoney
                 companys[0].Buy(buyMoney)
                 print("< 주식 구매가 성공적으로 이루어 졌습니다 >")
-        elif choose == '2' :
-            buyMoney = input("사실 금액을 적어주세요 >> ")
-            if buyMoney > mymoney :
+        elif choose == 2 :
+            buyMoney = int(input("사실 금액을 적어주세요 >> "))
+            if mymoney < buyMoney :
                 print("< 보유 금액이 부족합니다 >")
             else :
                 mymoney = mymoney - buyMoney
                 companys[1].Buy(buyMoney)
                 print("< 주식 구매가 성공적으로 이루어 졌습니다 >")
-        elif choose == '3' :
-            buyMoney = input("사실 금액을 적어주세요 >> ")
-            if buyMoney > mymoney :
+        elif choose == 3 :
+            buyMoney = int(input("사실 금액을 적어주세요 >> "))
+            if mymoney < buyMoney :
                 print("< 보유 금액이 부족합니다 >")
             else :
                 mymoney = mymoney - buyMoney
                 companys[2].Buy(buyMoney)
                 print("< 주식 구매가 성공적으로 이루어 졌습니다 >")
-        elif choose == '4' :
-            buyMoney = input("사실 금액을 적어주세요 >> ")
-            if buyMoney > mymoney :
+        elif choose == 4 :
+            buyMoney = int(input("사실 금액을 적어주세요 >> "))
+            if mymoney < buyMoney :
                 print("< 보유 금액이 부족합니다 >")
             else :
                 mymoney = mymoney - buyMoney
                 companys[3].Buy(buyMoney)
                 print("< 주식 구매가 성공적으로 이루어 졌습니다 >")
-        elif choose == '5' :
-            buyMoney = input("사실 금액을 적어주세요 >> ")
-            if buyMoney > mymoney :
+        elif choose == 5 :
+            buyMoney = int(input("사실 금액을 적어주세요 >> "))
+            if mymoney < buyMoney :
                 print("< 보유 금액이 부족합니다 >")
             else :
                 mymoney = mymoney - buyMoney
                 companys[4].Buy(buyMoney)
                 print("< 주식 구매가 성공적으로 이루어 졌습니다 >")
 
-def Sell():
-    print()
-    print("< 판매할 회사를 선택하여 주세요 >")
+def Sell() :
     for i in range(5) :
-        print(i+1 + " : " + companys[i].name)
-    choose = int(input(">> "))
-    if choose <= 0 and choose > 5 :
-        print("< 지정되지 않은 회사입니다 >")
-    else :
-        if choose == 1 :
-            if companys[0].buyCheck == 0 :
-                print("< 해당 회사를 사지 않았습니다 >")
-            else :
-                companys[0].change()
-                temp = 0
-                global mymoney 
-                if companys[0].money > companys[0].buyCheck:
-                    temp = companys[0].money - companys[0].buyCheck
-                    mymoney = mymoney - temp
-                    print("< 총 " + temp + "만큼의 손해를 입얻습니다 >")
-                else :
-                    temp = companys[0].buyCheck - companys[0].money
-                    
-                    mymoney = mymoney + temp + companys[0].buyCompany
-                    print("< 총 " + temp + companys[0].buyCompany + "만큼의 수익을 얻었습니다 >")
-                    
-        elif choose == 2:
-            if companys[1].buyCheck == 0 :
-                print("< 해당 회사를 사지 않았습니다 >")
-            else :
-                companys[1].change()
-                temp = 0
-                if companys[1].money > companys[1].buyCheck:
-                    temp = companys[1].money - companys[1].buyCheck
-                    global mymoney 
-                    mymoney = mymoney - temp
-                    print("< 총 " + temp + "만큼의 손해를 입얻습니다 >")
-                else :
-                    temp = companys[0].buyCheck - companys[1].money
-                    global mymoney
-                    mymoney = mymoney + temp + companys[1].buyCompany
-                    print("< 총 " + temp + companys[1].buyCompany + "만큼의 수익을 얻었습니다 >")
-        elif choose == 3 :
-            if companys[2].buyCheck == 0 :
-                print("< 해당 회사를 사지 않았습니다 >")
-            else :
-                companys[2].change()
-                temp = 0
-                if companys[2].money > companys[2].buyCheck:
-                    temp = companys[2].money - companys[2].buyCheck
-                    global mymoney 
-                    mymoney = mymoney - temp
-                    print("< 총 " + temp + "만큼의 손해를 입얻습니다 >")
-                else :
-                    temp = companys[2].buyCheck - companys[2].money
-                    global mymoney
-                    mymoney = mymoney + temp + companys[2].buyCompany
-                    print("< 총 " + temp + companys[2].buyCompany + "만큼의 수익을 얻었습니다 >")
-        elif choose == 4 :
-            if companys[3].buyCheck == 0 :
-                print("< 해당 회사를 사지 않았습니다 >")
-            else :
-                companys[3].change()
-                temp = 0
-                if companys[3].money > companys[3].buyCheck:
-                    temp = companys[3].money - companys[3].buyCheck
-                    global mymoney 
-                    mymoney = mymoney - temp
-                    print("< 총 " + temp + "만큼의 손해를 입얻습니다 >")
-                else :
-                    temp = companys[3].buyCheck - companys[3].money
-                    global mymoney
-                    mymoney = mymoney + temp + companys[3].buyCompany
-                    print("< 총 " + temp + companys[3].buyCompany + "만큼의 수익을 얻었습니다 >")
+        companys[i].change()
+    
+    for i in range(5) :
+        print(str(i+1) + " : " + companys[i].name)
+    choose = input("판매할 회사를 입력하여 주세요(1~5) >> ")
+    print()
+    if choose == '1' :
+        if companys[0].buyCompany == 0 :
+            print("< 해당 회사의 주식이 없습니다 >")
         else :
-            if companys[4].buyCheck == 0 :
-                print("< 해당 회사를 사지 않았습니다 >")
-            else :
-                companys[4].change()
-                temp = 0
-                if companys[4].money > companys[4].buyCheck:
-                    temp = companys[4].money - companys[4].buyCheck
-                    global mymoney 
-                    mymoney = mymoney - temp
-                    print("< 총 " + temp + "만큼의 손해를 입얻습니다 >")
-                else :
-                    temp = companys[4].buyCheck - companys[4].money
-                    global mymoney
-                    mymoney = mymoney + temp + companys[4].buyCompany
-                    print("< 총 " + temp + companys[4].buyCompany + "만큼의 수익을 얻었습니다 >")
-
-
-
+            global mymoney
+            mymoney = mymoney + int(companys[0].buyCompany + (companys[0].buyCheck - companys[0].upDown))
+            print(mymoney)
+    else :
+        print("< 존재하지 않는 회사입니다 >")
+     
 
 def main():
-    tutorial()
+    tuto = tutorial()
+    if tuto == "err" :
+        print("< 이용해 주셔서 감사합니다 >")
+        return 
     print("< 주식 게임을 시작합니다 >")
     print()
     while True:
-        print("현재 나의 자산 : " + mymoney)
+        print("현재 나의 자산 : " + str(mymoney))
         order = input("명령을 입력해주세요 >> ")
         if order == "companys" :
             Companys()
@@ -221,6 +144,8 @@ def main():
             Sell()
         else:
             print("< 정해지지 않은 명령입니다 >")
+            print()
+        
     
 
 main()
